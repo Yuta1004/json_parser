f4b058187ce48fe8b4f67375871fb1774ccbe483
NakagamiYuta <guguru0014@gmail.com>
2020-02-20 23:53:42 +0900

    [clean] 定義順を変更

--- src/parser/mtype/value.rs
+++ src/parser/mtype/value.rs
@@ -1,7 +1,7 @@
 enum Value {
     Number  { val: i32 },
-    Str     { val: String },
-    Bool    { val: bool }
+    Bool    { val: bool },
+    Str     { val: String }
 }
 
 impl Value {
@@ -9,27 +9,27 @@ impl Value {
         Value::Number { val }
     }
 
-    fn new_str(val: impl Into<String>) -> Value {
-        Value::Str { val: val.into() }
-    }
-
     fn new_bool(val: bool) -> Value {
         Value::Bool { val }
+    }
+
+    fn new_str(val: impl Into<String>) -> Value {
+        Value::Str { val: val.into() }
     }
 }
 
 #[test]
 fn test_value_new() {
     let n = Value::new_num(1204);
-    let s = Value::new_str("Gochiusa");
     let b = Value::new_bool(true);
+    let s = Value::new_str("Gochiusa");
     let v_list = vec![n, s, b];
 
     for value in v_list {
         match value {
             Value::Number { val } => assert_eq!(val, 1204),
-            Value::Str { val } => assert_eq!(val, "Gochiusa"),
-            Value::Bool { val } => assert!(val)
+            Value::Bool { val } => assert!(val),
+            Value::Str { val} => assert_eq!(val, "Gochiusa")
         }
     }
 }
